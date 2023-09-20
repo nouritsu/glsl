23bc76d52c2064627842812f0dd19fe53bda5b2b
Aneesh <aneesh1701@gmail.com>
2023-09-21 01:08:21 +0530

    add squares pattern shader, change values for better visualization

--- Patterns/squares.fs.glsl
+++ Patterns/squares.fs.glsl
@@ -1,16 +1,19 @@
+/* Main function, uniforms & utils */
 #ifdef GL_ES
 precision mediump float;
 #endif
 
 uniform vec2 u_resolution;
-uniform vec2 u_mouse;
 uniform float u_time;
 
 void main(){
     vec2 uv=(gl_FragCoord.xy*2.-u_resolution.xy)/u_resolution.y;
     
-    float radius=.5;
-    float d=length(uv)-radius;
+    float s=.3;
+    vec2 dim=abs(uv)-vec2(s);
+    float d=length(max(dim,0.))+min(max(dim.x,dim.y),0.);
+    d=step(.00001,d);
+    d=abs(sin(d+u_time));
     
     gl_FragColor=vec4(vec3(d),1.);
 }
--- random.fs.glsl
+++ random.fs.glsl
@@ -3,10 +3,38 @@ precision mediump float;
 #endif
 
 uniform vec2 u_resolution;
-uniform vec2 u_mouse;
 uniform float u_time;
 
+#define PI 3.14159265359
+
+float plot(vec2 st,float pct){
+    return smoothstep(pct-.02,pct,st.y)-
+    smoothstep(pct,pct+.02,st.y);
+}
+
+float r(float x){
+    return max(cos(x*2.*PI),0.);
+}
+
+float g(float x){
+    return max(sin(x*1.325*PI),0.);
+}
+
+float b(float x){
+    return max(sin(x*4.-.8),0.);
+}
+
 void main(){
-    vec2 st=gl_FragCoord.xy/u_resolution;
-    gl_FragColor=vec4(st.x,st.y,1.,1.);
+    vec2 uv=gl_FragCoord.xy/u_resolution;
+    vec3 col=vec3(uv.x,0.,0.);
+    
+    col.r=r(uv.x);
+    col.g=g(uv.x);
+    col.b=b(uv.x);
+    
+    // col=mix(col,vec3(1.,0.,0.),plot(uv,r(uv.x)));
+    // col=mix(col,vec3(0.,1.,0.),plot(uv,g(uv.x)));
+    // col=mix(col,vec3(0.,0.,1.),plot(uv,b(uv.x)));
+    
+    gl_FragColor=vec4(col,1.);
 }
